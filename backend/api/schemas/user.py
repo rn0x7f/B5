a9a972cffb653f3d7ca4308ff6c2036381a4dfb3
rn0x7f <ampheriz0@gmail.com>
2024-09-23 09:19:54 -0600

    update: EmailStr

--- backend/api/schemas/user.py
+++ backend/api/schemas/user.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class User(BaseModel):
-    correo_electronico: str
+    correo_electronico: EmailStr
     nombre: str
     apellido: str
     telefono: str
