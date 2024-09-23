1ddab8ad3e22034a682c754655dccd83a96873e3
rn0x7f <ampheriz0@gmail.com>
2024-09-23 14:33:48 -0600

    add: auth token login

--- backend/api/routes/auth.py
+++ backend/api/routes/auth.py
@@ -1,14 +1,66 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models.user import users
+from schemas.user import User
+from sqlalchemy.orm import Session
+from typing import List
+from sqlalchemy.exc import IntegrityError
+from config.db import get_db, SECRET_KEY
+import jwt
+from datetime import datetime, timedelta
+import bcrypt
 
 auth = APIRouter()
 
-@auth.post("/signin")
-async def signin():
-    return {"message": "Sign in"}
+# Función para hashear contraseñas
+def hash_password(password: str) -> str:
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-@auth.post("/signup")
-async def signup():
-    return {"message": "Sign up"}
+# Función para verificar contraseñas
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+# Función para crear un token JWT
+def create_token(user_id: int) -> str:
+    expiration = datetime.utcnow() + timedelta(hours=1)
+    token = jwt.encode({"user_id": user_id, "exp": expiration}, SECRET_KEY, algorithm="HS256")
+    return token
+
+@auth.post("/signin")
+async def signin(email: str, password: str, db: Session = Depends(get_db)):
+    # Buscar el usuario por correo electrónico
+    user = db.query(users).filter(users.c.correo_electronico == email).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+    # Verificar la contraseña
+    if not bcrypt.checkpw(password.encode('utf-8'), user.contrasena.encode('utf-8')):
+        raise HTTPException(status_code=401, detail="Inicio de sesion no valido.")
+
+    # Generar un token JWT
+    token = create_token(user.correo_electronico)  # Usa el correo electrónico
+    return {"token": token}
+
+
+@auth.post("/signup", response_model=User)
+async def signup(user_data: User, db: Session = Depends(get_db)):
+    new_user = users.insert().values(
+        correo_electronico=user_data.correo_electronico,
+        nombre=user_data.nombre,
+        apellido=user_data.apellido,
+        telefono=user_data.telefono,
+        contrasena=hash_password(user_data.contrasena)
+    )
+    
+    try:
+        db.execute(new_user)
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="El correo electrónico ya está registrado.")
+    
+    return user_data
+
 
 @auth.post("/signout")
 async def signout():
