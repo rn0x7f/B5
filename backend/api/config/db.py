a1749496cdd4c8bd2dce4fa34b87dced22cd39e1
rn0x7f <ampheriz0@gmail.com>
2024-09-22 23:42:23 -0600

    test: add -> db, conn error

--- backend/api/config/db.py
+++ backend/api/config/db.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine, MetaData
 from dotenv import load_dotenv
 import os
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 
 # Dos niveles hacia arriba para llegar al .env
 # Cargar las variables de entorno desde el archivo .env en la raíz del proyecto
@@ -24,3 +27,17 @@ meta = MetaData()
 
 # Conexion a la base de datos
 conn = engine.connect()
+
+
+Base = declarative_base()
+
+# Crear un SessionLocal para manejar sesiones
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Función para obtener una sesión de base de datos
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
