66ed5c5dcf6d3069b22a821515e40f05ef45e603
rn0x7f <ampheriz0@gmail.com>
2024-09-26 08:11:06 -0600

    add: get_user_by_email

--- backend/api/routes/usuario.py
+++ backend/api/routes/usuario.py
@@ -22,3 +22,9 @@ def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = UsuarioCrud.get_usuarios(db, skip=skip, limit=limit)
     return users
 
+@usuario.get("/users/{email}", response_model=Usuario)
+def get_user(email: str, db: Session = Depends(get_db)):
+    user = UsuarioCrud.get_usuario_by_email(db, email)
+    if user is None:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    return user
