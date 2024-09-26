bd96c348380800c01437a8c7965f76ad6b50f234
rn0x7f <ampheriz0@gmail.com>
2024-09-26 09:29:06 -0600

    update: usuario routes

--- backend/api/routes/usuario.py
+++ backend/api/routes/usuario.py
@@ -13,22 +13,26 @@ def get_db():
     finally:
         db.close()
 
-@usuario.post("/users/", response_model=Usuario)
+@usuario.post("/", response_model=Usuario)
 def create_user(usuario: UsuarioCreate, db: Session = Depends(get_db)):
     return UsuarioCrud.create_usuario(db=db, usuario=usuario)
 
-@usuario.get("/users/", response_model=list[Usuario])
+@usuario.get("/", response_model=list[Usuario])
 def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = UsuarioCrud.get_usuarios(db, skip=skip, limit=limit)
     return users
 
-@usuario.get("/users/{email}", response_model=Usuario)
+@usuario.get("/{email}", response_model=Usuario)
 def get_user(email: str, db: Session = Depends(get_db)):
     user = UsuarioCrud.get_usuario_by_email(db, email)
     if user is None:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return user
 
-@usuario.put("/users/{email}", response_model=Usuario)
+@usuario.put("/{email}", response_model=Usuario)
 def update_user(email: str, usuario: UsuarioCreate, db: Session = Depends(get_db)):
     return UsuarioCrud.update_usuario(db, email, usuario)
+
+@usuario.delete("/{email}")
+def delete_user(email: str, db: Session = Depends(get_db)):
+    return UsuarioCrud.delete_usuario(db, email)
