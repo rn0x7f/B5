788b439afb907a38509146f1fb4e8d1dd413e38c
rn0x7f <ampheriz0@gmail.com>
2024-09-27 14:38:54 -0600

    add: catalogo router

--- backend/api/app.py
+++ backend/api/app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from routes.usuario import usuario
 from routes.auth import auth
 from routes.producto import producto
+from routes.catalogo import catalogo
 
 app = FastAPI()
 
 app.include_router(usuario, prefix="/api/users", tags=["users"])
+app.include_router(catalogo, prefix="/api/catalogs", tags=["catalogs"])
 app.include_router(producto, prefix="/api/products", tags=["products"])
 app.include_router(auth, prefix="/api/auth", tags=["auth"])
