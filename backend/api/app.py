21bb67351ab5cfd09d7912608af1ec465d9ee052
rn0x7f <ampheriz0@gmail.com>
2024-09-27 13:40:44 -0600

    add: router productos

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from routes.usuario import usuario
-from routes.authUsuario import authUsuario
+from routes.auth import auth
+from routes.producto import producto
 
 app = FastAPI()
 
 app.include_router(usuario, prefix="/api/users", tags=["users"])
-app.include_router(authUsuario, prefix="/api/auth", tags=["auth"])
+app.include_router(producto, prefix="/api/products", tags=["products"])
+app.include_router(auth, prefix="/api/auth", tags=["auth"])
