afe7afbd67320bf4a8192918c0122c8c19b2649e
rogerenn <renrooger@gmail.com>
2024-10-02 16:41:33 -0600

    pantalla usuario

--- app/src/main/java/com/todasbrillamos/view/pantallas/CuentaScreen.kt
+++ app/src/main/java/com/todasbrillamos/view/pantallas/CuentaScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -19,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.todasbrillamos.R
+import com.todasbrillamos.view.componentes.CampoTexto
 import com.todasbrillamos.view.componentes.NavBar
 import com.todasbrillamos.view.componentes.TextoResaltado
 import com.todasbrillamos.view.componentes.boton
@@ -32,12 +35,16 @@ import com.todasbrillamos.view.componentes.boton
 
 @Composable
 fun CuentaScreen(navController: NavHostController) {
-
     // Definir gradiente
     val gradientColors = listOf(
         Color(0xFFffe5b4), // Color inicial
         Color(0xFFffbba8)  // Color final
     )
+
+    // Estado para los campos de texto
+    val nombre = remember { mutableStateOf("") }
+    val email = remember { mutableStateOf("") }
+    val telefono = remember { mutableStateOf("") }
 
     Scaffold(
         bottomBar = { NavBar(navController) }
@@ -49,9 +56,43 @@ fun CuentaScreen(navController: NavHostController) {
                 .padding(innerPadding)
                 .padding(28.dp)
         ) {
-            // Lista de opciones en forma de lista vertical
-            Column(){
+            // Campos de texto para el nombre, email y teléfono
+            CampoTexto(
+                labelValue = "Nombre",
+                painterResource = painterResource(id = R.drawable.profile),
+                textValue = nombre.value,
+                onValueChange = { nombre.value = it }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
+            CampoTexto(
+                labelValue = "Apellido",
+                painterResource = painterResource(id = R.drawable.profile),
+                textValue = nombre.value,
+                onValueChange = { nombre.value = it }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            CampoTexto(
+                labelValue = "Correo electrónico",
+                painterResource = painterResource(id = R.drawable.mail),
+                textValue = email.value,
+                onValueChange = { email.value = it }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            CampoTexto(
+                labelValue = "Teléfono",
+                painterResource = painterResource(id = R.drawable.phone),
+                textValue = telefono.value,
+                onValueChange = { telefono.value = it }
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+
+            // Botón para actualizar los datos
+            boton(value = "Actualizar datos") {
+                // Acción para actualizar los datos del usuario
+                // Aquí puedes añadir la lógica para actualizar los datos
             }
         }
     }
