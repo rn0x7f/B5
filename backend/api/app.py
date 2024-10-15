3f8b7904459d43ead61dc4f2b8d539e7847a3350
rn0x7f <ampheriz0@gmail.com>
2024-10-15 15:38:35 -0600

    init: compra route

--- backend/api/app.py
+++ backend/api/app.py
@@ -6,6 +6,7 @@ from routes.producto import producto
 from routes.catalogo import catalogo
 from routes.stripe import stripe_route
 from routes.carrito import carrito
+from routes.compra import compra
 
 app = FastAPI()
 
@@ -26,5 +27,6 @@ app.include_router(usuario, prefix="/api/users", tags=["users"])
 app.include_router(catalogo, prefix="/api/catalogs", tags=["catalogs"])
 app.include_router(producto, prefix="/api/products", tags=["products"])
 app.include_router(carrito, prefix="/api/carts", tags=["carts"])
+app.include_router(compra, prefix="/api/purchases", tags=["purchases"])
 app.include_router(auth, prefix="/api/auth", tags=["auth"])
 app.include_router(stripe_route, prefix="/api/stripe", tags=["stripe"])
---
+++ backend/api/utils/compraCrud.py
@@ -0,0 +1,4 @@
+from sqlalchemy.orm import Session
+from schemas import schemas
+from models import models
+from fastapi import HTTPException
